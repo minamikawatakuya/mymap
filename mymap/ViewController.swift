54c8070d3542d0714b085fd14c0a7df17ec4c1a4
minamikawatakuya <55489051+minamikawatakuya@users.noreply.github.com>
2020-09-25 13:27:51 +0900

    最初のコミット

---
+++ mymap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mymap
+//
+//  Created by minamikawa on 2020/09/25.
+//  Copyright © 2020 minamikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
