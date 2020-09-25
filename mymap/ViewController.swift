825988e09fce345819d1c67317d7f9b038dda816
minamikawatakuya <55489051+minamikawatakuya@users.noreply.github.com>
2020-09-25 14:55:56 +0900

    拡大・縮小表示

--- mymap/ViewController.swift
+++ mymap/ViewController.swift
@@ -11,15 +11,37 @@ import CoreLocation
 import MapKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
+    var myLock = NSLock()
+    let goldenRatio = 1.618
+    
     @IBOutlet var mapView: MKMapView!
     var locationManager: CLLocationManager!
 
     @IBAction func clickZoomin(_ sender: Any) {
         print("[DBG]clickZoomin")
+        myLock.lock()
+        if (0.005 < mapView.region.span.latitudeDelta / goldenRatio) {
+            print("[DBG]latitudeDelta-1 : " + mapView.region.span.latitudeDelta.description)
+            var regionSpan:MKCoordinateSpan = MKCoordinateSpan()
+            regionSpan.latitudeDelta = mapView.region.span.latitudeDelta / goldenRatio
+            mapView.region.span = regionSpan
+            print("[DBG]latitudeDelta-2 : " + mapView.region.span.latitudeDelta.description)
+        }
+        myLock.unlock()
     }
     
     @IBAction func clickZoomout(_ sender: Any) {
         print("[DBG]clickZoomout")
+        myLock.lock()
+        if (mapView.region.span.latitudeDelta * goldenRatio < 150.0) {
+            print("[DBG]latitudeDelta-1 : " + mapView.region.span.latitudeDelta.description)
+            var regionSpan:MKCoordinateSpan = MKCoordinateSpan()
+            regionSpan.latitudeDelta = mapView.region.span.latitudeDelta * goldenRatio
+            //regionSpan.latitudeDelta = mapView.region.span.longitudeDelta * GoldenRatio
+            mapView.region.span = regionSpan
+            print("[DBG]latitudeDelta-2 : " + mapView.region.span.latitudeDelta.description)
+        }
+        myLock.unlock()
     }
     
     override func viewDidLoad() {
@@ -42,7 +64,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         print("[DBG]longitude : " + longitude)
         print("[DBG]latitude : " + latitude)
         
+        myLock.lock()
         mapView.setCenter((locations.last?.coordinate)!, animated: true)
+        myLock.unlock()
     }
 
 
