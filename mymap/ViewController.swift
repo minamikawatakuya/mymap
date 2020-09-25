a60217c9cc9ef570df6ff50dccca972c6b28dd29
minamikawatakuya <55489051+minamikawatakuya@users.noreply.github.com>
2020-09-25 13:58:48 +0900

    位置情報取得

--- mymap/ViewController.swift
+++ mymap/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    var locationManager: CLLocationManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()  // 変数を初期化
+        locationManager.delegate = self  // delegateとしてself(自インスタンス)を設定
+
+        locationManager.startUpdatingLocation()  // 位置情報更新を指示
+        locationManager.requestWhenInUseAuthorization()  // 位置情報取得の許可を得る
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+        let longitude = (locations.last?.coordinate.longitude.description)!
+        let latitude = (locations.last?.coordinate.latitude.description)!
+        print("[DBG]longitude : " + longitude)
+        print("[DBG]latitude : " + latitude)
     }
 
 
