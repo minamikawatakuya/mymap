4f668497f046a92b914541014e7288cf82dee389
minamikawatakuya <55489051+minamikawatakuya@users.noreply.github.com>
2020-09-29 17:42:50 +0900

    xibファイルなど作成

---
+++ mymap/PlaceCell.swift
@@ -0,0 +1,32 @@
+//
+//  PlaceCell.swift
+//  mymap
+//
+//  Created by minamikawa on 2020/09/29.
+//  Copyright © 2020 minamikawa. All rights reserved.
+//
+
+import UIKit
+
+class PlaceCell: UITableViewCell {
+    
+    @IBOutlet weak var idField: UITextField!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var addressField: UITextField!
+    @IBOutlet weak var identifierField: UITextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func pushUpdate(_ sender: Any) {
+    }
+    
+}
