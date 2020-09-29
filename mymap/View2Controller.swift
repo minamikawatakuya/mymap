8d7974b37bc53d5271cd65df4b4768abbfca153c
minamikawatakuya <55489051+minamikawatakuya@users.noreply.github.com>
2020-09-29 18:11:24 +0900

    Delegateなど

--- mymap/View2Controller.swift
+++ mymap/View2Controller.swift
@@ -1,7 +1,8 @@
 
 import UIKit
+import RealmSwift
 
-class View2Controller: UIViewController {
+class View2Controller: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var idField: UITextField!
     @IBOutlet weak var nameField: UITextField!
@@ -10,12 +11,41 @@ class View2Controller: UIViewController {
     
     @IBOutlet weak var table: UITableView!
     
+    // モデルクラスを使用し、取得データを格納する変数を作成
+    var tableCells: Results<Place>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
     @IBAction func pushRegist(_ sender: Any) {
+    }
+    
+    // cellの数を指定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tableCells.count
+    }
+
+    // cellに値を設定
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlaceCell",
+        for: indexPath) as! PlaceCell
+        
+        let tmpCell: Place = self.tableCells[(indexPath as NSIndexPath).row];
+        
+        cell.idField.text = tmpCell.id
+        cell.nameField.text = tmpCell.name
+        cell.addressField.text = tmpCell.address
+        cell.identifierField.text = tmpCell.identifier
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 225
     }
     
 }
